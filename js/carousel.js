7de30149ce781d0360d4981090ce8f229aea5819
jasitu-perkins <120684662+jasitu-perkins@users.noreply.github.com>
2023-08-23 00:31:15 -0400

    consolidated slick from html

--- js/carousel.js
+++ js/carousel.js
@@ -59,3 +59,26 @@ $("#slick").on("init", function () {
   document.querySelectorAll("audio").forEach((el) => (el.tabIndex = "-1"));
   document.querySelector(`#audio-${0}`).tabIndex = "0";
 });
+
+$("#slick").on("afterChange", function () {
+  const transcripts = [
+  "“My vision is very unpredictable. It likes to do what it wants. It gets frustrating because I want to be able to see things, I want to be able to participate in things, but sometimes it’s hard for me because my vision just completely goes at the wrong time! For me, my vision can go from probably visually impaired to only seeing movement, light, shadows, and color... I guess kinda like Monet paintings. Very distorted. I wish people understood more that our brains decide when it wants to work, visually, and when it wants to “peace out”, I’m done for the day.”", 
+  "“If I’m feeling tired, I like to relax. If I’m feeling energetic, I like to play video games. And then, if I’m in an upset mood, I just take a vision break. If I’m at school, I’d just ask the teacher and if she says yes, I either take a walk or put my head down on the desk. It sometimes helps and sometimes stays the same, but it never gets worse.”",
+  "“I do art, painting sometimes, at home. I paint postcards. It’s not easy to do because it’s a small little square thing, but it comes out really well... usually! I mostly like painting when I’m at my house. I like to paint how I feel.”", 
+  "“I do well with color. Like, color in terms of color contrast. They help me distinguish things, like cartoon characters that I’ve seen a bunch of times, like Scooby Doo and Mickey and Winnie the Pooh, and things that I have come before. When I see them, I have a mental picture of them in my head, so I know how to map them. Yeah, it’s like a landmark, almost.”", 
+  "“A lot of people call that a white cane, so... I call it an extender because I think of it as extending my vision. I always thought of it like, “OK, it’s extending my vision, so it’s an extend<em>er</em>. And not a lot of people think of it that way.<br><br>But a lot of people probably don’t understand that just because you see someone with a white cane — or extender as I like to call it — doesn’t mean that they are completely blind. There are different levels of blindness; you may have a white cane, but you’re not completely blind, necessarily.”", 
+  "Zeke: On a random note, can I just say something random? A man with a blue puppet, once said, “Pizza is a circle that is cut into triangles that comes in a square”! [I like] really, really bad jokes.<br>Why did the boy throw a stick of butter? Because he wanted to see a butter-fly<br><br>Another voice: There we go! <em>laughter</em>"]
+
+  const transcriptTitles = [
+  "Tina's vision is like a Monet painting",
+  "Sometimes, Omer needs a vision break",
+  "Aidan likes to paint watercolors",
+  "Color is like a landmark for Micah",
+  "Kyle calls his white cane an “extender” because it extends his vision",
+  "Zeke likes to tell jokes"    
+  ]
+
+  var dataId = $(".slick-current").attr("data-slick-index");
+  document.querySelector('#cvi-audio-transcript').innerHTML = transcripts[dataId]
+  document.querySelector('#cvi-audio-transcript-header').innerHTML = transcriptTitles[dataId]
+});
